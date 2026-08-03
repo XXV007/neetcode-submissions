27f3546567075bb2ffd5c9df94f6ac879f341dfb
Vishnu Priyan Bhaskar <114556663+XXV007@users.noreply.github.com>
2026-08-02 17:51:05 -0700

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+
+        freq = Counter(tasks)
+        max_freq = max(freq.values())
+        max_count = sum(1 for v in freq.values() if v == max_freq)
+
+        intervals = (max_freq - 1) * (n + 1) + max_count
+
+        return max(len(tasks), intervals)
+        
