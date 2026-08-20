941f26ff741630d1d6caabecaac0e9bdaeb0dc43
Vishnu Priyan Bhaskar <114556663+XXV007@users.noreply.github.com>
2026-08-19 19:50:23 -0700

    Add: foreign-dictionary - submission-0

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-0.py
@@ -0,0 +1,35 @@
+from collections import defaultdict, deque
+from typing import List
+
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+
+        adj = defaultdict(set)
+        in_degree = {c: 0 for word in words for c in word}
+
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            min_len = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:min_len] == w2[:min_len]:
+                return ""
+            for j in range(min_len):
+                if w1[j] != w2[j]:
+                    if w2[j] not in adj[w1[j]]:
+                        adj[w1[j]].add(w2[j])
+                        in_degree[w2[j]] += 1
+                    break
+        
+        queue = deque([c for c in in_degree if in_degree[c] == 0])
+        res = []
+        while queue:
+            c = queue.popleft()
+            res.append(c)
+            for nei in adj[c]:
+                in_degree[nei] -= 1
+                if in_degree[nei] == 0:
+                    queue.append(nei)
+        
+        if len(res) < len(in_degree):
+            return ""
+        return "".join(res)
+        
